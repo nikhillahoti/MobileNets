379ee0c9b097441c1e05c45f8e6b109ffd0ba2dc
Nikhil Lahoti <lahotinikhil92@gmail.com>
2018-12-24 13:02:03 -0800

    Changes to the Python Script to check if the two output are the same

--- Python Code/First Layer/CheckingFirstLayer.py
+++ Python Code/First Layer/CheckingFirstLayer.py
@@ -1,34 +1,44 @@
 
 
 counter = 0
-fMy = open("output.txt", "r")
+fMy = open("data/LayerOne/output.txt", "r")
 myContent = fMy.readlines()
 fAc = open("First_Layer_Output.txt", "r")
 acContent = fAc.readlines()
 
 iterr = -1
+iterrMy = -1
 position = []
+mismatchValues = []
+countZeros = 0
 for i in range(0, 32):
-    for j in range(0, 112):
-        for k in range(0, 112):
-            iterr += 1
-            #if k > 95: continue
+    for j in range(0, 114):
+        for k in range(0, 114):
 
-            if int(float(myContent[iterr].strip())) == int(float(acContent[iterr].strip())):
+            iterrMy += 1
+
+            if k > 111 or j > 111:
+                if float(myContent[iterrMy]).is_integer(): countZeros += 1
+                continue
+
+            iterr += 1
+
+            if int(round(float(myContent[iterrMy].strip()))) == int(round(float(acContent[iterr].strip()))):
                 counter += 1
             else:
                 position.append((i,j,k))
-                print(myContent[iterr].strip())
-                print(acContent[iterr].strip())
-                print("----")
+                mismatchValues.append((myContent[iterrMy].strip(), acContent[iterr].strip()))
 fMy.close()
 fAc.close()
-print("Total Match ", counter)
-print(iterr)
-print(len(position))
+print("Total Match ---> ", counter)
+print("Total Mismatch ---> " + str(len(position)))
+print(iterrMy)
+print(countZeros)
 
-for i in range(len(position)):
-    print(position[i])
-#for i in range(10):
+#for i in range(len(position)):
 #    print(position[i])
+if len(position) > 10:
+    for i in range(len(position)):
+        print(position[i])
+        print(mismatchValues[i])
 
